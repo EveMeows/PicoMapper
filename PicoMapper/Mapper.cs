c97b8cd326c44afb85a0c7c973a37671d058d4c0
Evelyn Serra <143884194+EveMeows@users.noreply.github.com>
2025-03-09 20:06:21 +0200

    Add project files.

---
+++ PicoMapper/Mapper.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PicoMapper;
+
+public class Mapper : Game
+{
+    private GraphicsDeviceManager graphics;
+    private SpriteBatch spriteBatch = null!;
+
+    public Mapper()
+    {
+        this.graphics = new GraphicsDeviceManager(this);
+        this.Content.RootDirectory = "Content";
+        
+        this.IsMouseVisible = true;
+        this.Window.AllowUserResizing = true;
+    }
+
+    protected override void LoadContent()
+    {
+        this.spriteBatch = new SpriteBatch(this.GraphicsDevice);
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        this.GraphicsDevice.Clear(Color.Black);
+        base.Draw(gameTime);
+    }
+}
